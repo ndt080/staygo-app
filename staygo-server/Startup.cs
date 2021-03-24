03f243d5f5022983a83d28a45bbdf9bb9eb03d67
varff <62174550+varff@users.noreply.github.com>
2021-03-24 17:57:30 +0300

    Partly Fixed
    Still no mapping cause idnt understand which info would it get and which data should it return

--- staygo-server/Startup.cs
+++ staygo-server/Startup.cs
@@ -16,6 +16,7 @@ namespace staygo_server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,7 +28,7 @@ namespace staygo_server
             }
 
             app.UseRouting();
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
