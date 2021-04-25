af6b425703d11342035f070489c1d1110e63c815
varff <62174550+varff@users.noreply.github.com>
2021-04-25 14:18:46 +0300

    Bar class update

--- StayGo.Frontend/iOSApp/Models/Bar.swift
+++ StayGo.Frontend/iOSApp/Models/Bar.swift
@@ -1,12 +1,20 @@
 import Foundation
 
 class Bar{
+    var ID : Int
     var name : String
+    var address : String
     var description : String
+    var type : String
     var rate : Int
+    var avgCheck : Double
     init (){
+        self.ID = 0
+        self.address = ""
+        self.type = ""
         self.name = "Bar"
         self.description = "Super puper bar"
         self.rate = 0
+        self.avgCheck = 0
     }
 }
